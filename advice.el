d648dfc477b558823622b3c9647a23e49a174471
László Vaskó <laszlo.vasko@outlook.com>
2019-11-10 17:13:46 +0100

    adding a prefix to logs emitted by the adjusted installer

--- advice.el
+++ advice.el
@@ -1,10 +1,10 @@
 ;;; -*- lexical-binding: t; -*-
 (advice-add 'nix-straight-get-used-packages
             :before (lambda (&rest r)
-                      (message "Advising doom installer to gather packages to install...")
+                      (message "[nix-doom-emacs] Advising doom installer to gather packages to install...")
                       (advice-add 'doom-reload-autoloads
                                   :override (lambda (&optional file force-p)
-                                              (message "Skipping generating autoloads...")))
+                                              (message "[nix-doom-emacs] Skipping generating autoloads...")))
                       (advice-add 'doom--format-print
                                   :around (lambda (orig-print &rest r)
                                             (let ((noninteractive nil))
@@ -12,5 +12,5 @@
 
 (advice-add 'y-or-n-p
             :override (lambda (q)
-                        (message "%s \n--> answering NO" q)
+                        (message "%s \n[nix-doom-emacs] --> answering NO" q)
                         nil))

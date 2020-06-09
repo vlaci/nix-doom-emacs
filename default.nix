2ab836b8f4fb29be307ef65fd925fe2e1b7925bc
László Vaskó <laszlo.vasko@outlook.com>
2020-06-10 01:26:08 +0200

    extracting overrides to its own file
    This is the most volatile part of the code base, default.nix should be
    fairly stable by now.

--- default.nix
+++ default.nix
@@ -58,78 +58,7 @@ let
             then dependencyOverrides.${p}
             else sources.${p};
   # Packages we need to get the default doom configuration run
-  overrides = self: super: {
-    evil-escape = super.evil-escape.overrideAttrs (esuper: {
-      patches = [ ./evil-escape.patch ];
-    });
-    straightBuild = { pname, ... }@args: self.trivialBuild ({
-      ename = pname;
-      version = "1";
-      src = lock pname;
-      buildPhase = ":";
-    } // args);
-    doom-snippets = self.straightBuild {
-      pname = "doom-snippets";
-    };
-    evil-markdown = self.straightBuild {
-      pname = "evil-markdown";
-    };
-    evil-org = self.straightBuild {
-      pname = "evil-org-mode";
-    };
-    evil-quick-diff = self.straightBuild {
-      pname = "evil-quick-diff";
-    };
-    magit = super.magit.overrideAttrs (esuper: {
-      preBuild = ''
-        make VERSION="${esuper.version}" -C lisp magit-version.el
-      '';
-    });
-    org-mode = self.straightBuild rec {
-      pname = "org-mode";
-      version = "9.4";
-      installPhase = ''
-        LISPDIR=$out/share/emacs/site-lisp
-        install -d $LISPDIR
-
-        cp -r * $LISPDIR
-
-        cat > $LISPDIR/lisp/org-version.el <<EOF
-        (fset 'org-release (lambda () "${version}"))
-        (fset 'org-git-version #'ignore)
-        (provide 'org-version)
-        EOF
-      '';
-    };
-    org-yt = self.straightBuild {
-      pname = "org-yt";
-    };
-    php-extras = self.straightBuild {
-      pname = "php-extras";
-    };
-    rotate-text = self.straightBuild {
-      pname = "rotate-text.el";
-    };
-    so-long = self.straightBuild {
-      pname = "emacs-so-long";
-    };
-    ob-racket = self.straightBuild {
-      pname = "ob-racket";
-    };
-
-    # dune has a nontrivial derivation, which does not buildable from the melpa
-    # wrapper falling back to the one in nixpkgs
-    dune = pkgs.dune.overrideAttrs (old: {
-      # Emacs derivations require an ename attribute
-      ename = old.pname;
-
-      # Need to adjust paths here match what doom expects
-      postInstall = ''
-        mkdir -p $out/share/emacs/site-lisp/editor-integration
-        ln -snf $out/share/emacs/site-lisp $out/share/emacs/site-lisp/editor-integration/emacs
-      '';
-    });
-  };
+  overrides = pkgs.callPackage ./overrides.nix { inherit lock; };
 
   # Stage 1: prepare source for byte-compilation
   doomSrc = stdenv.mkDerivation {
